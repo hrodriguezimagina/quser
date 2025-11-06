56fb099c873205c862ffe6b16442e24acc784353
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-11-06 18:28:46 -0500

    + added firebase get token

--- _store/auth/actions.js
+++ _store/auth/actions.js
@@ -11,6 +11,8 @@ import apiResponse from '@imagina/qcrud/_plugins/apiResponse'
 import axios from 'axios'
 import config from '@imagina/qsite/_config/master/index'
 import {uid} from 'quasar'
+import { getTokenFirebase } from '@imagina/qnotification/_plugins/firebase.js'
+
 
 //Request Login
 export const AUTH_REQUEST = ({commit, dispatch, state}, authData) => {
@@ -66,6 +68,8 @@ export const AUTH_SUCCESS = ({commit, dispatch, state}, data = false) => {
         await cache.set('sessionData', data)//Save session data in storage
         commit('SET_AUTHENTICATED')
         await dispatch('SET_ORGANIZATION')//Set settings
+        await getTokenFirebase(data.userData.id);//Get firebase token for notifications
+        
         return resolve(true)//Resolve
       } else {
         console.info('[AUTH_SUCCESS]::LOGOUT')
